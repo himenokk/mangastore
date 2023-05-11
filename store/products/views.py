caa6f44e75081f55c1f3ce4712ff8d9a8a4b09a4
Sanzhar Kiyassov <kiyassovs@gmail.com>
2023-05-11 22:38:08 +0600

    DRY principles and dynamic urls

--- store/products/views.py
+++ store/products/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'tittle': 'Mangastore',
+        'username': 'Sanzhar',
+    }
+    return render(request, 'products/index.html', context)
 
 def about(request):
     return render(request, 'products/about.html')
@@ -10,4 +14,29 @@ def news(request):
     return render(request, 'products/news.html')
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Mangastore - products',
+        'products': [
+            {
+                'image': '/static/img/aot1.jpg',
+                'name': 'Attack on Titan Vol. 1',
+                'price': 2000,
+                'description': 'In this post-apocalyptic sci-fi story, humanity has been devastated by the bizarre, giant humanoids known as the Titans',
+            },
+            {
+                'image': '/static/img/spy9.jpg',
+                'name': 'Spy x Family, Vol. 9',
+                'price': 1800,
+                'description': 'An action-packed comedy about a fake family that includes a spy, an assassin and a telepath!',
+            },
+            {
+                'image': '/static/img/vinlandsaga1.jpeg',
+                'name': 'Vinland Saga 1',
+                'price': 2500,
+                'description': 'Vinland saga  is a Japanese historical manga series written and illustrated by Makoto Yukimura.',
+            },
+
+        ]
+
+    }
+    return render(request, 'products/products.html', context)
