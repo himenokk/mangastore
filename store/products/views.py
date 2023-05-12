14cdf38cea457796f9526c67d7fe397f2ccb97de
Sanzhar Kiyassov <kiyassovs@gmail.com>
2023-05-12 20:57:02 +0600

    goods and categories submitted in bd, created json files

--- store/products/views.py
+++ store/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from products.models import Product, ProductCategory
+
 
 # Create your views here.
 def index(request):
@@ -16,27 +18,7 @@ def news(request):
 def products(request):
     context = {
         'title': 'Mangastore - products',
-        'products': [
-            {
-                'image': '/static/img/aot1.jpg',
-                'name': 'Attack on Titan Vol. 1',
-                'price': 2000,
-                'description': 'In this post-apocalyptic sci-fi story, humanity has been devastated by the bizarre, giant humanoids known as the Titans',
-            },
-            {
-                'image': '/static/img/spy9.jpg',
-                'name': 'Spy x Family, Vol. 9',
-                'price': 1800,
-                'description': 'An action-packed comedy about a fake family that includes a spy, an assassin and a telepath!',
-            },
-            {
-                'image': '/static/img/vinlandsaga1.jpeg',
-                'name': 'Vinland Saga 1',
-                'price': 2500,
-                'description': 'Vinland saga  is a Japanese historical manga series written and illustrated by Makoto Yukimura.',
-            },
-
-        ]
-
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
--- store/store/urls.py
+++ store/store/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from products.views import index, about, news, products
 
@@ -24,5 +26,11 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('news/', news, name='news'),
     path('products/', products, name='products')
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
