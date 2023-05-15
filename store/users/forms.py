793e90d54b10e49d41f3abd48e3b6a028055cd8b
Sanzhar Kiyassov <kiyassovs@gmail.com>
2023-05-16 00:24:09 +0600

    profile

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from django import forms
 
 from users.models import User
@@ -9,6 +9,37 @@ class UserLoginForm(AuthenticationForm):
         'class': 'form-control py-4', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control py-4', 'placeholder': 'Password'}))
+
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter your name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter your surname'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Choose a username'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Enter your email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Choose a password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Confirm your password'}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-2'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-2'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-2', 'readonly': True}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-2', 'readonly': True}))
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
--- store/users/views.py
+++ store/users/views.py
@@ -1,11 +1,9 @@
-
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm
-
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -25,4 +23,19 @@ def login(request):
 
 
 def registration(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+
+    return render(request, 'users/register.html', context)
+
+
+def profile(request):
+    form = UserProfileForm()
+    context = {'title': 'Mangastore - profile', 'form': form}
+    return render(request, 'users/profile.html', context)
